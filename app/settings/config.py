4410c01f1d78dfca7ec9f922036b4cbea0349fc1
Ant1dot7 <fybrby98@mail.ru>
2024-07-22 14:57:53 +0300

    update avatar

--- app/settings/config.py
+++ app/settings/config.py
@@ -13,9 +13,6 @@ class Settings(BaseSettings):
     jwt_key: str = Field(alias='JWT_KEY')
     jwt_alg: str = Field(alias='JWT_ALG')
 
-    minio_root_user: str = Field(alias='MINIO_ROOT_USER')
-    minio_root_password: str = Field(alias='MINIO_ROOT_PASSWORD')
-
     aws_access_key_id: str = Field(alias='AWS_ACCESS_KEY_ID')
     aws_secret_access_key: str = Field(alias='AWS_SECRET_ACCESS_KEY')
     s3_url: str = Field(alias='S3_URL')
